df5271a69198edbc4b5c3046b9ba98f98e98c329
Dhiru Vadher <vadherdhiraj8@gmail.com>
2022-02-13 14:49:20 +0530

    Updates

---
+++ precache-manifest.20030cfff366a5aabc27a0f3d5fcadb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d921948fb57b95514346",
+    "url": "/test13/static/css/main.c1f4dbb3.chunk.css"
+  },
+  {
+    "revision": "d921948fb57b95514346",
+    "url": "/test13/static/js/main.7fd5334b.chunk.js"
+  },
+  {
+    "revision": "5ccb47a50a2961fc6d37",
+    "url": "/test13/static/js/runtime~main.96d3ff87.js"
+  },
+  {
+    "revision": "3a04364beb6b016caca9",
+    "url": "/test13/static/js/2.49326cf8.chunk.js"
+  },
+  {
+    "revision": "ea3458a47561fd227043ff912dbbb116",
+    "url": "/test13/index.html"
+  }
+];
